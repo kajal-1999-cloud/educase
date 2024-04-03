a3299cd8c4e6a9b43eb0c5d1b71d4ec11206477d
kajalg1401 <kajalg1401@gmail.com>
2024-04-03 20:06:50 +0530

    error resolved

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router,Routes,  Route } from 'react-router-dom';
+import Home from './components/HomePage';
+import Login from './components/Login';
+import Signup from './components/CreateAcct';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Router>
+   <Routes>
+          <Route exact path="/" Component={Home} />
+          <Route path="/about" Component={Login} />
+          <Route path="/contact" Component={Signup} />
+          <Route Component={Profile} /> {/* This route will be rendered if no other route matches */}
+    </Routes>
+   </Router>
     </div>
   );
 }
